ee18e56ccfe200ae4ffb2a6b3d65541c4278c0f2
sEGuoer <709074535@qq.com>
2023-09-15 09:06:58 +0800

    spring

---
+++ src/main/java/com/guoer/demo/Greeting.java
@@ -0,0 +1,3 @@
+package com.guoer.demo;
+
+public record Greeting(long id, String content) { }
---
+++ src/test/java/com/guoer/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
